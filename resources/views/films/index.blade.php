29a9d45368f3cd69fa968c5dc5357fed01f99f3d
Ghadeer <ghaora@gmail.com>
2022-03-09 20:48:16 +0200

    table of films in view index

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.base')
 
 @section('content')
-    @dump($films)
+    
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Thumb</th>
+      <th scope="col">Price</th>
+      <th scope="col">Series</th>
+      <th scope="col">Sale Date</th>
+      <th scope="col">Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($films as $film)
+    <tr>
+      <th scope="row">{{$film->id}}</th>
+      <td>{{$film->title}}</td>
+      <td>{{$film->description}}</td>
+      <td><img src="{{$film->thumb}}" alt=""></td>
+      <td>{{$film->price}}</td>
+      <td>{{$film->series}}</td>
+      <td>{{$film->sale_date}}</td>
+      <td>{{$film->type}}</td>
+    </tr>
+    @endforeach
 @endsection
+
+  </tbody>
+</table>
