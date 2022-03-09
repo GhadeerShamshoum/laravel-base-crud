78ffb51b3bf5a2f56df6605fbd7d1a30ab7e15de
Ghadeer <ghaora@gmail.com>
2022-03-09 16:45:31 +0200

    first commit

---
+++ database/migrations/2022_03_09_142000_create_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string("title",20);
+            $table->text("description");
+            $table->string("thumb")->nullable();
+            $table->float("price",5,2);
+            $table->string("series",30);
+            $table->date("sale_date");
+            $table->string("type",20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
